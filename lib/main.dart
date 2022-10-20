f762cb31a9cffee52fb55b6d680d073aea4171b1
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-20 23:23:11 +0500

    Making a purple Box using Container()

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text("Hello WOrld"),
+        body: Container(
+          height: 100,
+          width: 100,
+          color: Colors.purple,
+        ),
       ),
     );
   }
