6cbd3f70d9196e46e47d07e6d7f61b9b8bcdde5e
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-21 23:54:56 +0500

    CrossAxisAlignment.end

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text("ABC"),
             Text("DEF"),
