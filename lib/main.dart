a6f1501043d3c38c754c6cd7ffb34215b431a69a
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-23 00:02:22 +0500

    SingleChildScrollView() for scrolling down the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -11,71 +11,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.purple,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.green,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.pink,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.pink,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.pink,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.pink,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.pink,
-              ),
-            ],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.purple,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.pink,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.pink,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.pink,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.pink,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.pink,
+                ),
+              ],
+            ),
           ),
         ),
       ),
